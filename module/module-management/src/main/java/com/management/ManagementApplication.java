41f8e9d0da2c94f3e87de5fa82f3c55a32513819
Kaiser-Lee <39914654+Kaiser-Li@users.noreply.github.com>
2018-09-07 17:52:56 +0800

    开发环境之---dubbo+zk环境搭建

--- module/module-management/src/main/java/com/management/ManagementApplication.java
+++ module/module-management/src/main/java/com/management/ManagementApplication.java
@@ -1,5 +1,28 @@
 package com.management;
 
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class ManagementApplication {
+
+    public static void main(String[] args){
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+
+                    "classpath:/provider-dubbo.xml"
+
+        );
+        try{
+            System.out.println("management MicroServce provided, main thead wait!");
+            byte[] b = new byte[0];
+            synchronized(b){
+                b.wait();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        } finally{
+            ctx.close();
+            ctx.destroy();
+        }
+
+    }
 }
--- web/src/main/java/com/management/XwebApplication.java
+++ web/src/main/java/com/management/XwebApplication.java
@@ -4,13 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource({"classpath:/spring-dubbo.xml"})
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-public class XwebAppliction {
+public class XwebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(XwebAppliction.class,args);
+        SpringApplication.run(XwebApplication.class,args);
         System.out.println("ヾ(◍°∇°◍)ﾉﾞ    启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
                 " ______                    _   ______            \n" +
                 "|_   _ \\                  / |_|_   _ `.          \n" +
