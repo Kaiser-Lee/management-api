533604da359930aef395aee1872c379be1d47d83
DESKTOP-02UICPJ\卢方谱 <18650018670@163.com>
2018-09-19 22:48:34 +0800

    登录验证初步

--- common/common-config/src/main/java/com/management/brower/BrowerSecurityConfig.java
+++ common/common-config/src/main/java/com/management/brower/BrowerSecurityConfig.java
@@ -1,9 +1,23 @@
 package com.management.brower;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Logger;
 
 @Configuration
 public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private org.slf4j.Logger logger = LoggerFactory.getLogger(BrowerSecurityConfig.class);
+
+    protected void configure (HttpSecurity http) throws Exception {
+        http.formLogin()                         // 当定义需要登录的用户的时候 跳转到登录界面
+                .and()
+                .authorizeRequests()            // 定义那些url需要被保护 哪些不需要被保护
+                .anyRequest()                   //任何请求登录之后都可以访问
+                .authenticated();
+    }
 }
