c2265084722301b13f8ac53a8b0bfe11cb9d77b5
Kaiser-Lee <39914654+Kaiser-Li@users.noreply.github.com>
2018-09-20 09:44:05 +0800

    开发环境之---dubbo+zk环境搭建

--- web/src/main/java/com/management/common/config/BrowerSecurityConfig.java
+++ web/src/main/java/com/management/common/config/BrowerSecurityConfig.java
@@ -14,7 +14,7 @@ public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure (HttpSecurity http) throws Exception {
         http.formLogin()                         // 当定义需要登录的用户的时候 跳转到登录界面
                 .and()
-                .authorizeRequests()            // 定义那些url需要被保护 哪些不需要被保护
+                .authorizeRequests()            // 定义那些url需要被保护 哪些不需要被保护 测试
                 .anyRequest()                   //任何请求登录之后都可以访问
                 .authenticated();
     }
